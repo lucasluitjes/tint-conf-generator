f8be24fd8c826ec7319bfd0dce57a968425f780f
Justin-Koningsberger <justin.koningsberger@gmail.com>
2021-03-24 15:24:15 +0100

    Fix indentation, remove clutter

--- generate_conf.rb
+++ generate_conf.rb
@@ -1,23 +1,19 @@
 require 'erb'
 
 class Button
-	include ERB::Util
-  attr_accessor :items, :template
-
-
-	def initialize(number, text, action, template)
-		@number = number
-		@text = text
-		@lclick = action
+  def initialize(number, text, action, template)
+    @number = number
+    @text = text
+    @lclick = action
     @template = template
-	end
+  end
 
   def render
     ERB.new(@template).result(binding)
   end
 
   def show
-  	puts render
+    puts render
   end
 end
 
@@ -48,39 +44,38 @@ def switch_panel(p)
   "DISPLAY=':1' ruby generate_conf.rb #{p} && killall -SIGUSR1 tint2"
 end
 
-panels = {
-  normal: {
-    up: key("Up"),
-    copy: key("Control_L+c"),
-    paste: key("Control_L+v"),
-    down: key("Down"),
-    enter: key("Return"),
-    close_tab: key("Control_L+w"),
-    backspace: key("BackSpace"),
-    wasd_panel: switch_panel(:wasd)
-  },
-  wasd: {
-    w: key("w"),
-    a: key("a"),
-    s: key("s"),
-    d: key("d"),
-    normal_panel: switch_panel(:normal)
-  }
-}
-
-
 def generate_conf(tint_buttons)
   static_content = File.open('static_tint_conf').read
   File.write('generated_tint2_conf', static_content)
   File.open('generated_tint2_conf', 'a') do |f|
     tint_buttons.each_with_index do |(key, value), index|
       f << Button.new(
-          index + 1, "-      #{key}",
-          value, button_template
-        ).render
+        index + 1, "-      #{key}",
+        value, button_template
+      ).render
     end
   end
 end
+
+panels = {
+  normal: {
+    up: key('Up'),
+    copy: key('Control_L+c'),
+    paste: key('Control_L+v'),
+    down: key('Down'),
+    enter: key('Return'),
+    close_tab: key('Control_L+w'),
+    backspace: key('BackSpace'),
+    wasd_panel: switch_panel(:wasd)
+  },
+  wasd: {
+    w: key('w'),
+    a: key('a'),
+    s: key('s'),
+    d: key('d'),
+    normal_panel: switch_panel(:normal)
+  }
+}
 
 if ARGV[0]
   generate_conf(panels[ARGV[0].to_sym])
